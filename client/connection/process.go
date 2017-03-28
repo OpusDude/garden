e422e626b3930e94a1a881ff10368cde228be7a6
Ed King <eking@pivotal.io>
2017-03-28 17:50:49 +0200

    Client implementation of ExitStatus()
    [#129069765]
    
    Signed-off-by: Petar Petrov <pppepito86@gmail.com>

--- client/connection/process.go
+++ client/connection/process.go
@@ -1,26 +1,19 @@
 package connection
 
-import (
-	"sync"
-
-	"code.cloudfoundry.org/garden"
-)
+import "code.cloudfoundry.org/garden"
 
 type process struct {
 	id string
 
 	processInputStream *processStream
-	done               bool
-	exitStatus         int
-	exitErr            error
-	doneL              *sync.Cond
+	status             chan garden.ProcessStatus
 }
 
 func newProcess(id string, processInputStream *processStream) *process {
 	return &process{
 		id:                 id,
 		processInputStream: processInputStream,
-		doneL:              sync.NewCond(&sync.Mutex{}),
+		status:             make(chan garden.ProcessStatus, 1),
 	}
 }
 
@@ -28,16 +21,13 @@ func (p *process) ID() string {
 	return p.id
 }
 
+func (p *process) ExitStatus() chan garden.ProcessStatus {
+	return p.status
+}
+
 func (p *process) Wait() (int, error) {
-	p.doneL.L.Lock()
-
-	for !p.done {
-		p.doneL.Wait()
-	}
-
-	defer p.doneL.L.Unlock()
-
-	return p.exitStatus, p.exitErr
+	ret := <-p.status
+	return ret.Code, ret.Err
 }
 
 func (p *process) SetTTY(tty garden.TTYSpec) error {
@@ -48,12 +38,8 @@ func (p *process) Signal(signal garden.Signal) error {
 	return p.processInputStream.Signal(signal)
 }
 
-func (p *process) exited(exitStatus int, err error) {
-	p.doneL.L.Lock()
-	p.exitStatus = exitStatus
-	p.exitErr = err
-	p.done = true
-	p.doneL.L.Unlock()
-
-	p.doneL.Broadcast()
+func (p *process) exited(exitStatus garden.ProcessStatus) {
+	//the exited function should only be called once otherwise the
+	//line below will block
+	p.status <- exitStatus
 }
